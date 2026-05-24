92270718986843707dfed3feeb603b0dcbab129f
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-05-24 18:23:28 +0100

    Add random joke generator script

---
+++ experiments/random_joke_generator.py
@@ -0,0 +1,9 @@
+import random
+
+jokes = [
+    "Why do programmers prefer dark mode? Because light attracts bugs.",
+    "Why did Python go to school? To improve its class.",
+    "Why was the computer cold? It forgot to close its windows."
+]
+
+print(random.choice(jokes))
