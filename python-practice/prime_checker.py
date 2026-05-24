6b0aa3ba7ccab1ba9a8f806db7d69b1790a076ca
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-05-24 18:15:15 +0100

    Add prime number checker in prime_checker.py

---
+++ python-practice/prime_checker.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number: "))
+
+if number > 1:
+    for i in range(2, number):
+        if number % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
+else:
+    print("Not a prime number")
