46fbba32576a2e9adc7d6a46f3ba357d1b61c28b
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-05 15:38:01 +0100

    Add random fact generator script

---
+++ experiments/random_fact_generator.py
@@ -0,0 +1,9 @@
+import random
+
+facts = [
+    "Python was released in 1991.",
+    "Git was created by Linus Torvalds.",
+    "AI stands for Artificial Intelligence."
+]
+
+print(random.choice(facts))
