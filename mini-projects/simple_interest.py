22ffe8add4f4fe0520dc68d4ec7155a24bcfa76c
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-07 17:38:33 +0100

    Add simple interest calculation script

---
+++ mini-projects/simple_interest.py
@@ -0,0 +1,7 @@
+principal = float(input("Principal: "))
+rate = float(input("Rate: "))
+time = float(input("Time: "))
+
+interest = (principal * rate * time) / 100
+
+print("Simple Interest:", interest)
