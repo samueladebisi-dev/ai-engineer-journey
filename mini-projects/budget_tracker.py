8304868f1b7d14d02c766c83ab87a82cbc684cca
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-12 13:14:52 +0100

    Add balance evaluation messages to budget tracker

--- mini-projects/budget_tracker.py
+++ mini-projects/budget_tracker.py
@@ -4,3 +4,10 @@ expenses = float(input("Enter expenses: "))
 balance = income - expenses
 
 print("Remaining balance:", balance)
+
+if balance > 0:
+    print("You are saving money.")
+elif balance < 0:
+    print("You spent more than you earned.")
+else:
+    print("You broke even.")
