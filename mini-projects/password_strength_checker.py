b613ee97ebcaa2d035563d6d63472ce392dc1982
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-14 11:43:36 +0100

    Update password strength criteria to require 12 characters

--- mini-projects/password_strength_checker.py
+++ mini-projects/password_strength_checker.py
@@ -1,6 +1,8 @@
 password = input("Enter password: ")
 
-if len(password) >= 8:
+if len(password) >= 12:
     print("Strong Password")
+elif len(password) >= 8:
+    print("Moderate Password")
 else:
     print("Weak Password")
