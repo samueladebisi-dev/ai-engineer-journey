a5b54d1abefde1989b47ffcd7c3bc1b8a38abe61
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-16 07:37:01 +0100

    Add percentage calculation to BMI calculator

--- mini-projects/bmi_calculator_app.py
+++ mini-projects/bmi_calculator_app.py
@@ -1,6 +1,10 @@
 weight = float(input("Weight (kg): "))
 height = float(input("Height (m): "))
+obtained = float(input("Marks obtained: "))
+total = float(input("Total marks: "))
 
 bmi = weight / (height ** 2)
+percentage = (obtained / total) * 100
 
 print("BMI:", round(bmi, 2))
+print("Percentage:", round(percentage, 2))
