4db9a75731680f3aea85064f7dfbe7b750f3f722
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-17 11:35:30 +0100

    Add print statements for balance and withdrawal

--- mini-projects/atm_simulator.py
+++ mini-projects/atm_simulator.py
@@ -1,9 +1,12 @@
 balance = 5000
 
+print("Current Balance:", balance)
+
 amount = float(input("Enter withdrawal amount: "))
 
 if amount <= balance:
     balance -= amount
+    print("Withdrawal successful")
     print("Remaining balance:", balance)
 else:
     print("Insufficient funds")
