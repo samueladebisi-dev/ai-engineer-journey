af4e54b5c1a3ebb4e64892772bccdbd7a8239884
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-17 11:39:25 +0100

    Add random GitHub tips script

---
+++ experiments/random_github_tip.py
@@ -0,0 +1,10 @@
+import random
+
+tips = [
+    "Commit consistently.",
+    "Write clear README files.",
+    "Build projects regularly.",
+    "Document your learning."
+]
+
+print(random.choice(tips))
