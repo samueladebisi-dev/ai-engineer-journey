c1c23ba21636259b003f30ecf329ad6953133a71
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-26 06:33:14 +0100

    Implement basic expense tracker functionality

---
+++ mini-projects/expense_trackerv1.py
@@ -0,0 +1,24 @@
+expenses = []
+
+while True:
+    print("\n1. Add Expense")
+    print("2. View Expenses")
+    print("3. Exit")
+
+    choice = input("Choose an option: ")
+
+    if choice == "1":
+        name = input("Expense name: ")
+        amount = float(input("Amount: "))
+
+        expenses.append({
+            "name": name,
+            "amount": amount
+        })
+
+    elif choice == "2":
+        for expense in expenses:
+            print(f"{expense['name']} - ₦{expense['amount']}")
+
+    elif choice == "3":
+        break
