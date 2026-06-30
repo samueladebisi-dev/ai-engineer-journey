1c51050fb283450291bb42a9e881eec8e896cea7
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-06-30 19:45:00 +0100

    added initial unit tests

---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+import unittest
+
+# from self.assertTrue(True).utils import save_json
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_module_import(self):
+        # self.assertTrue(callable(save_json))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
