2d98f71c764d51cd0945f4a7284faa24fd31af3b
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-07-03 07:17:35 +0100

    added type hints to json utilities

--- mini-projects/utils.py
+++ mini-projects/utils.py
@@ -1,10 +1,11 @@
 import json
+from typing import Any
 
-def save_json(filename, data):
+def save_json(filename: str, data: list[Any]) -> None:
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
 
-def load_json(filename):
+def load_json(filename: str) -> list:
     with open(filename, "r") as file:
         return json.load(file)
