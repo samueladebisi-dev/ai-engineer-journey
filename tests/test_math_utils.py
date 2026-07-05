a82e00fa36b42d89b9473c235174b93c23856751
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-07-05 19:33:11 +0100

    expanded math unit tests

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,9 +1,17 @@
 import unittest
 
+
 class TestMath(unittest.TestCase):
 
-    def test_addition(self):
-        self.assertEqual(2 + 3, 5)
+    def test_add(self):
+        self.assertEqual(10 + 5, 15)
+
+    def test_subtract(self):
+        self.assertEqual(10 - 5, 5)
+
+    def test_multiply(self):
+        self.assertEqual(5 * 4, 20)
+
 
 if __name__ == "__main__":
     unittest.main()
