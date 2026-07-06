cf7b4104dc5dca56b5f70485d332c5118600b6a0
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-07-06 05:26:29 +0100

    added docstrings to json utilities

--- mini-projects/utils.py
+++ mini-projects/utils.py
@@ -2,10 +2,14 @@ import json
 from typing import Any
 
 def save_json(filename: str, data: list[Any]) -> None:
+    """Save Python data to a JSON file."""
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
-
 def load_json(filename: str) -> list:
-    with open(filename, "r") as file:
-        return json.load(file)
+    """Load data from a JSON file."""
+    try:
+        with open(filename, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
