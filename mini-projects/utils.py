156fc2d2e1f5bfac1b3c5430e21296cf6cdc46c6
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-07-07 07:51:32 +0100

    introduced expense type alias

--- mini-projects/utils.py
+++ mini-projects/utils.py
@@ -1,12 +1,14 @@
 import json
 from typing import Any
 
-def save_json(filename: str, data: list[Any]) -> None:
+Expense = dict[str, Any]
+
+def save_json(filename: str, data: list[Expense]) -> None:
     """Save Python data to a JSON file."""
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
-def load_json(filename: str) -> list:
+def load_json(filename: str) -> list[Expense]:
     """Load data from a JSON file."""
     try:
         with open(filename, "r") as file:
