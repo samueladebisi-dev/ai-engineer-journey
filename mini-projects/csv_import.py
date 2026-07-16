141e9db4982bd326a195bc9cf67573d916b70865
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-07-16 11:04:10 +0100

    added csv import utility

---
+++ mini-projects/csv_import.py
@@ -0,0 +1,19 @@
+import csv
+
+
+def import_expenses(filename: str) -> list[dict]:
+    expenses = []
+
+    with open(filename, "r", newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            expenses.append(
+                {
+                    "name": row["name"],
+                    "category": row["category"],
+                    "amount": float(row["amount"]),
+                }
+            )
+
+    return expenses
