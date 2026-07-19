2e1e8c808a3646f8900870c7194deb85799dd810
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-07-19 18:40:00 +0100

    added expense manager tests

---
+++ tests/test_expense_manager.py
@@ -0,0 +1,22 @@
+import unittest
+from expense_manager import ExpenseManager
+
+
+class TestExpenseManager(unittest.TestCase):
+
+    def test_add(self):
+        manager = ExpenseManager([])
+
+        manager.expenses.append(
+            {
+                "name": "Food",
+                "category": "Food",
+                "amount": 100,
+            }
+        )
+
+        self.assertEqual(len(manager.all()), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
