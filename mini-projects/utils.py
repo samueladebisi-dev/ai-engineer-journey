64ee6b7cc81a1dee78abd3228ce0e3ee5f00b583
Adebisi Samuel <oluwatoyinadebisi93@gmail.com>
2026-07-19 18:10:00 +0100

    improved utility type hints

--- mini-projects/utils.py
+++ mini-projects/utils.py
@@ -3,12 +3,12 @@ from typing import Any
 
 Expense = dict[str, Any]
 
-def save_json(filename: str, data: list[Expense]) -> None:
+def save_json(filename: str, data: list[dict]) -> None:
     """Save Python data to a JSON file."""
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
-def load_json(filename: str) -> list[Expense]:
+def load_json(filename: str) -> list[dict]:
     """Load data from a JSON file."""
     try:
         with open(filename, "r") as file:
